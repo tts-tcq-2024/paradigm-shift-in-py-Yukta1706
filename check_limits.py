b4ed9a69993246f28006ddb5e8fb6dbb6a84f29d
Yukta1706 <75121611+Yukta1706@users.noreply.github.com>
2024-08-27 16:04:03 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,4 +1,4 @@
-from battery_check import battery_is_ok
+from Temperature_check import battery_is_ok
 if __name__ == '__main__':
     assert battery_is_ok(25, 70, 0.7) is True
     assert battery_is_ok(50, 85, 0) is False
